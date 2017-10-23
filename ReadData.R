772dfea4173af5b9b7108373b5759f002a1210ca
nattsp <drnphillips@gmail.com>
2017-10-23 17:07:01 +1100

    Quiz questions right

--- ReadData.R
+++ ReadData.R
@@ -1,6 +1,8 @@
 #Read in files
 library(stringr)
 library(dplyr)
+library(tidyverse)
+library(readr)
 
 con <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.twitter.txt", "r")
 readLines(con, 1) ## Read the first line of text
@@ -23,6 +25,18 @@ while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
 print(maxLength)
 close(con) ## It's important to close the connection when you are done
 
+con <- file("en_US.twitter_short.txt", "rb")
+number_lines <- 0
+maxLength <- 0
+while (length(oneLine <- read_lines(con)) > 0) {
+  #line <- readLines(con, 1)
+  print(oneLine)
+  maxLength <- max(str_length(line), maxLength)
+  number_lines <- number_lines + 1
+}
+print(maxLength)
+print(number_lines)
+close(con) ## It's important to close the connection when you are done
 
 con <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.twitter.txt", "r")
 maxLengthUStwitter <- 0
@@ -52,6 +66,29 @@ while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
 print(maxLengthUSblogs)
 close(con) ## It's important to close the connection when you are done
 
+## Question 3 again
+
+con <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.blogs.txt", "rb")
+maxLengthUSblogs <- 0
+numberLines <- 0
+while (length(oneLine <- read_lines(con)) > 0) {
+  #line <- read_lines(con, 1)
+  maxLengthUSblogs <- max(str_length(oneLine), maxLengthUSblogs)
+  numberLines <- numberLines + 1
+}
+print(maxLengthUSblogs)
+print(numberLines)
+close(con) ## It's important to close the connection when you are done
+
+con <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.news.txt", "rb")
+maxLengthUSnews <- 0
+numberLines <- 0
+while (length(oneLine <- read_lines(con)) > 0) {
+  #line <- readLines(con, 1)
+  maxLengthUSnews <- max(str_length(oneLine), maxLengthUSnews)
+}
+print(maxLengthUSnews)
+close(con) ## It's important to close the connection when you are done
 
 ## Question 4
 
@@ -86,10 +123,26 @@ close(con)
 
 
 con <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.twitter.txt", "r")
+countQuote <- 0
+countLine <- 0
+while (length(oneLine <- readLines(con, n = 1, skipNul = TRUE, warn = FALSE)) > 0) {
+  line <- readLines(con, 1, skipNul = TRUE)
+  countLine <- countLine + 1
+  if (str_detect(line, "A computer once beat me at chess, but it was no match for me at kickboxing")){
+    print(line)
+    countQuote <- countQuote + 1
+  }
+}
+close(con)
+print(countQuote)
+print(countLine)
+
+
+con <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.twitter.txt", "r")
 countLine <- 0
 while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
-  line <- readLines(con, 1)
-  if (str_detect(line, "A computer once beat me at chess, but it was no match for me at kickboxing")){
+  line <- readLines(con, 1, skipNul = TRUE)
+  if (str_detect(line, "A computer once beat me at chess")){
     print(line)
     countLine <- countLine + 1
   }
@@ -97,14 +150,18 @@ while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
 close(con)
 print(countLine)
 
+
 con <- file("..\\..\\Data\\Coursera-SwiftKey\\final\\en_US\\en_US.twitter.txt", "r")
+countQuote <- 0
 countLine <- 0
-while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
-  line <- readLines(con, 1)
-  if (str_detect(line, "A computer once beat me at chess")){
+while (length(oneLine <- readLines(con, n = 1, skipNul = TRUE, warn = FALSE)) > 0) {
+  line <- readLines(con, 1, skipNul = TRUE)
+  countLine <- countLine + 1
+  if (str_detect(line, "A computer once beat me at chess, but it was no match for me at kickboxing")){
     print(line)
-    countLine <- countLine + 1
+    countQuote <- countQuote + 1
   }
 }
 close(con)
+print(countQuote)
 print(countLine)
