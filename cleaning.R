a115fd916772f1b8a3137772cf04b44cbf532129
nattsp <drnphillips@gmail.com>
2017-10-29 19:40:03 +1100

    table of frequency of words against frequency from each source "bag of words"

--- cleaning.R
+++ cleaning.R
@@ -5,6 +5,7 @@ library(dplyr)
 library(tidyverse)
 library(tidytext)
 library(stringr)
+library(ggplot2)
 
 ## Stopwords
 
@@ -38,8 +39,86 @@ SampleDatatidy$text <- str_replace_all(SampleDatatidy$text, '[^\\w ]', ' ')
 ## convert to lower case
 SampleDatatidy$text <- tolower(SampleDatatidy$text)
 
-sampleDataWords <- sampleDataWithRowTibble %>%
-    unnest_tokens(word, text)
+sampleDataTwittWords <- SampleDatatidy %>%
+    unnest_tokens(word, text) %>%
+    mutate(sample = "Twitter") %>%
+    select(sample, word) %>%
+    count(sample, word, sort = TRUE) %>%
+    ungroup()
+
+#mutate(sampleDataTwittWords, sample = "Twitter")
+
+#sampleData <- sampleDataWithRowTibble %>%
+#    unnest_tokens(word, text)
+
+
+
+### blogs
+
+
+sampleBlogsTidy <- sampleBlogsT %>%
+    mutate(text = str_replace_all(text, '\\d', ' ')) %>%
+    mutate(text = str_replace_all(text, '[^\\w ]', ' ')) %>%
+    mutate(text = tolower(text))
+
+sampleBlogsWords <- sampleBlogsTidy %>%
+    unnest_tokens(word, text) %>%
+    mutate(sample = "Blogs") %>%
+    select(sample, word) %>%
+    count(sample, word, sort = TRUE) %>%
+    ungroup()
+
+### news
+
+sampleNewsTidy <- sampleNewsT %>%
+    mutate(text = str_replace_all(text, '\\d', ' ')) %>%
+    mutate(text = str_replace_all(text, '[^\\w ]', ' ')) %>%
+    mutate(text = tolower(text))
+
+sampleNewsWords <- sampleNewsTidy %>%
+    unnest_tokens(word, text) %>%
+    mutate(sample = "News") %>%
+    select(sample, word) %>%
+    count(sample, word, sort = TRUE) %>%
+    ungroup()
+
+
+### combine
+
+sampleWords <- bind_rows(
+    sampleDataTwittWords
+    , sampleBlogsWords
+    , sampleNewsWords)
+
+totalWords <- sampleWords %>% 
+    group_by(sample) %>% 
+    summarize(total = sum(n))
+
+sampleWords <- left_join(sampleWords, totalWords)
+
+
+##plot
+
+
+
+ggplot(sampleWords, aes(n/total, fill = sample)) +
+    geom_histogram(show.legend = FALSE) +
+    xlim(NA, 0.000009) +
+    facet_wrap(~sample, ncol = 2, scales = "free_y")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 sampleDataWords <- sampleDataWords %>%
     anti_join(stop_words)
